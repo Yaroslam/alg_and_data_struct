a4be874374dff775a6cf089d7e44c94845962e08
Yaroslam <vestnik700@gmail.com>
2022-03-22 22:03:45 +0300

    start black red tree

---
+++ data_struct/tree/black_red_tree.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+struct Node_black_red{
+    int color;
+    int data;
+    Node_black_red* left;
+    Node_black_red* right;
+    Node_black_red* parent;
+};
+
